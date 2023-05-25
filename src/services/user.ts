3a433a651d7acff01bd86ef490e6f7f6b874ffcd
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-05-25 23:29:22 +0700

    Fix auth service

---
+++ src/services/user.ts
@@ -0,0 +1,25 @@
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { BaseResponse } from '@/types/common'
+import { User } from '@/types/user'
+import { fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const api = createApi({
+  reducerPath: 'users',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http:',
+  }),
+
+  keepUnusedDataFor: 259200,
+  endpoints: (builder) => ({
+    getProfile: builder.query<BaseResponse<User>, void>({
+      query: () => ({
+        method: 'GET',
+        url: `//localhost:3001/users/profile`,
+      }),
+    }),
+  }),
+})
+
+export const { useGetProfileQuery, useLazyGetProfileQuery } = api
+
+export default api
