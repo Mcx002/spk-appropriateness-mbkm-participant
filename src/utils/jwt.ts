18a6760b573209382c6fe1c14148a7633d09288f
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-06-03 20:35:31 +0700

    Adjust auth data utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,16 @@
-export const decodeJwtToken = (token: string): { header: any; payload: { full_name: string; user_type: string } } => {
+import { toast } from 'react-hot-toast'
+
+export const decodeJwtToken = (
+  token: string
+): {
+  header: any
+  payload: { name: string; email: string; user_type: string; additional_info: { identifier_id: string } }
+} => {
   const splitted = token.split('.')
   const jwtHeader = splitted[0]
   const jwtPayload = splitted[1]
   if (!jwtHeader || !jwtPayload) {
-    console.error('invalid jwt token')
+    toast.error('Invalid token')
   }
 
   const base64Header = JSON.parse(window.atob(jwtHeader))
