c89c69f755139446f0a30d1ac2e2306c284b886f
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-06-03 20:30:15 +0700

    Fix auth module

---
+++ src/layouts/Dashboard-student-layout.tsx
@@ -0,0 +1,26 @@
+import { FC, useEffect, useState } from 'react'
+import { getUserSession, UserRole } from '@/utils/auth'
+import { useRouter } from 'next/router'
+import Forbidden from '@/modules/errors/403'
+import { ReactNodeLike } from 'prop-types'
+import DashboardLayout from '@/layouts/Dashboard-layout'
+
+type Props = {
+  children: ReactNodeLike
+  title: string
+}
+
+const DashboardStudentLayout: FC<Props> = ({ children, title }) => {
+  const router = useRouter()
+  const [isProhibited, setProhibited] = useState(true)
+  useEffect(() => {
+    const user = getUserSession()
+    if (user && user.role === UserRole.Student) {
+      setProhibited(false)
+    }
+  }, [])
+
+  return isProhibited ? <Forbidden /> : <DashboardLayout title={title}>{children}</DashboardLayout>
+}
+
+export default DashboardStudentLayout
