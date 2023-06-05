b136a18371064b7bd2ae702c924a1e7dc88a9df6
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-06-06 00:00:21 +0700

    Add period services

--- src/services/period.ts
+++ src/services/period.ts
@@ -2,7 +2,7 @@ import { createApi } from '@reduxjs/toolkit/dist/query/react'
 
 import { baseQueryWithReauth } from '@/utils/api'
 import { BaseResponse } from '@/types/common'
-import { PeriodResponse } from '@/types/period'
+import { GetPeriodRequest, GetSpkResultRequest, PeriodDto, SpkResponse } from '@/types/period'
 
 const api = createApi({
   reducerPath: 'period',
@@ -11,15 +11,55 @@ const api = createApi({
   tagTypes: ['Period'],
   keepUnusedDataFor: 259200, // 3 days
   endpoints: (builder) => ({
-    getOpenPeriod: builder.query<BaseResponse<PeriodResponse>, void>({
+    getOpenPeriod: builder.query<BaseResponse<PeriodDto>, void>({
       query: () => ({
         method: 'GET',
         url: '/submission/open-period',
       }),
     }),
+    getListPeriod: builder.query<BaseResponse<PeriodDto[]>, GetPeriodRequest>({
+      query: () => ({
+        method: 'GET',
+        url: '/submission-period',
+      }),
+    }),
+    savePeriod: builder.mutation<BaseResponse<PeriodDto>, FormData>({
+      query: (formData) => ({
+        method: 'POST',
+        url: '/submission-period',
+        body: formData,
+      }),
+    }),
+    updatePeriod: builder.mutation<BaseResponse<PeriodDto>, FormData>({
+      query: (formData) => ({
+        method: 'PUT',
+        url: '/submission-period',
+        body: formData,
+      }),
+    }),
+    getSpkResult: builder.query<BaseResponse<SpkResponse[]>, GetSpkResultRequest>({
+      query: (data) => ({
+        method: 'GET',
+        url: `/submission/spk-result/${data.periodId}`,
+        params: data.params,
+      }),
+    }),
+    getDetailPeriod: builder.query<BaseResponse<PeriodDto>, string>({
+      query: (id) => ({
+        method: 'GET',
+        url: `/submission-period/${id}`,
+      }),
+    }),
   }),
 })
 
-export const { useGetOpenPeriodQuery } = api
+export const {
+  useGetOpenPeriodQuery,
+  useLazyGetListPeriodQuery,
+  useSavePeriodMutation,
+  useUpdatePeriodMutation,
+  useLazyGetSpkResultQuery,
+  useLazyGetDetailPeriodQuery,
+} = api
 
 export default api
