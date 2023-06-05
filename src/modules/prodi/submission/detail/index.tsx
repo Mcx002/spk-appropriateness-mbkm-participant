3928ecaffb233429926f9cc85499fe84a450a9a4
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-06-06 00:01:11 +0700

    Fix submission lecture module

---
+++ src/modules/prodi/submission/detail/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+import React from 'react'
+import SubmissionDetail from '@/modules/prodi/submission/detail/SubmissionDetail'
+
+const Page: NextPage = () => {
+  return <SubmissionDetail />
+}
+
+export default Page
